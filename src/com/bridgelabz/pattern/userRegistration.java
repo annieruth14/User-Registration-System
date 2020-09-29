2e61e9a351bde8798cd9cb355e4eeef9f6e8cfb3
annie <ruthannie3@gmail.com>
2020-09-29 12:06:44 +0530

    [Annie]Added: Welcome message

---
+++ src/com/bridgelabz/pattern/userRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.pattern;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class userRegistration {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration System ");
+		
+	}
+}
