6d900d0cbc8cdfc982c8f29fc359560a21ac0d22
annie <ruthannie3@gmail.com>
2020-09-29 15:06:21 +0530

    [Annie]Added: Pattern for validating last name

--- src/com/bridgelabz/pattern/userRegistration.java
+++ src/com/bridgelabz/pattern/userRegistration.java
@@ -8,7 +8,6 @@ public class userRegistration {
 	Scanner sc = new Scanner(System.in);
 	
 	public void check_firstName() {
-		   boolean result = false;
 		   System.out.println("Enter first name");  
 		   String value = sc.nextLine();
 		   
@@ -20,9 +19,22 @@ public class userRegistration {
 		   else 
 			   System.out.println("Invalid first name");
 	   }
+	public void check_lastName() {
+		System.out.println("Enter last name");  
+		   String value = sc.nextLine();
+		   
+		   String pattern = "^[A-Z][a-z]{2,}$";
+		   Pattern r = Pattern.compile(pattern);
+		   Matcher m = r.matcher(value);
+		   if (m.find())
+			   	System.out.println("Valid last name");
+		   else 
+			   System.out.println("Invalid last name");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration System ");
 	    userRegistration obj = new userRegistration();
 	    obj.check_firstName();
+	    obj.check_lastName();
 	    }
 }
